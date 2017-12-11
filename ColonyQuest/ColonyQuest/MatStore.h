fb661c1bc29eadbe30e5dbc8922d451df5c28fd6
GA1AB-gamedev <kcomgamedev1ab@dev.com>
2017-12-11 12:10:38 +0900

    構造体ヘッダーの作成

--- ColonyQuest/ColonyQuest/MatStore.h
+++ ColonyQuest/ColonyQuest/MatStore.h
@@ -1,25 +1,15 @@
 #pragma once
+#include "Struct.h"
 
 //構造体-----------
-//素材マテリアル
-//アイテムの名前と説明文、購入金額を管理
-struct Material
-{
-	//アイテム名
-	char Itemname[256];
-	//アイテムの説明
-	char Desc[256];
-	//購入金額（これを用いて売却値段も設定。売却値段は購入金額の半分）
-	unsigned int Money;
-};
 
 
 
-class MyClass
+typedef class CMatStore
 {
 public:
-	MyClass();
-	~MyClass();
+	
+	
 
 private:
 
---
+++ ColonyQuest/ColonyQuest/Struct.h
@@ -0,0 +1,43 @@
+#pragma once
+//マスター構造体ファイル
+//マスター構造体をここにぶち込む
+//使い方
+/*
+//XX構造体-------
+struct XX
+{
+	int XXXX;
+	char XXXX;
+	char XXXX;
+};
+*/
+
+//アイテム構造体
+//アイテムの名前と説明文、購入金額を管理
+struct Material
+{
+	//アイテム名
+	char Itemname[256];
+	//アイテムの説明
+	char Itemdesc[256];
+	//金額（これを用いて売却値段も設定。売却値段は購入金額の半分）
+	unsigned int Money;
+};
+
+//キャラクター構造体
+//キャラクター名、攻撃力、防御力、体力などのステータスを管理
+struct CharStats
+{
+	//キャラクター名
+	char Charname[256];
+	//キャラの体力
+	int Hp;
+	//キャラのスキルポイント
+	int Sp;
+	//攻撃力
+	int Atk;
+	//防御力
+	int Def;
+	//スキル力
+	int Spp;
+};
