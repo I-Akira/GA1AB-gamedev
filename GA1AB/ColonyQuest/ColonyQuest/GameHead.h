eebe95af74e1af4b781f84dcc256525b4b7fcce1
roa12017 <h1201719004@hiratagakuen.onmicrosoft.com>
2018-01-29 16:21:43 +0900

    commit

---
+++ GA1AB/ColonyQuest/ColonyQuest/GameHead.h
@@ -0,0 +1,91 @@
+#pragma once
+
+//オブジェクトネーム------------------------------
+enum OBJ_NAME
+{
+	OBJ_NO_NAME,	//オブジェクト名無し(禁止)
+	//ゲームで使うオブジェクトの名前
+	//OBJ_○○と表記
+	OBJ_TITLE,
+	OBJ_HERO,
+	OBJ_HEROATTACK,
+	OBJ_ENEMY,
+	OBJ_BLOCK,
+	OBJ_BATORU,
+	OBJ_SCENE_MAP,
+	OBJ_SHOP_ITEM,
+	OBJ_SHOP_SOUBI,
+	OBJ_SHOP_SOUBI2,
+	OBJ_JUMPENEMY,
+
+};
+//------------------------------------------------
+
+//当たり判定属性----------------------------------
+enum HIT_ELEMENTS
+{
+	ELEMENT_NULL,//属性無し 全ての属性と当たり判定が実行される
+	//以下　同じ属性同士では当たり判定は実行されない
+	//属性は追加可能だが、デバック時の色は初期設定分しか無い
+	ELEMENT_PLAYER,
+	ELEMENT_ENEMY,
+	ELEMENT_ITEM,
+	ELEMENT_MAGIC,
+	ELEMENT_FIELD,
+	ELEMENT_RED,
+	ELEMENT_GREEN,
+	ELEMENT_BLUE,
+	ELEMENT_BLACK,
+	ELEMENT_WHITE,
+};
+//------------------------------------------------
+
+//------------------------------------------------
+//セーブ＆ロードとシーン間のやり取りするデータ
+struct UserData
+{
+	int mSeveData;	//サンプルセーブデータ
+	
+};
+//------------------------------------------------
+
+
+//ゲーム内で使用されるグローバル変数・定数・列挙--
+
+
+
+//------------------------------------------------
+//ゲーム内で使用するクラスヘッダ------------------
+
+
+//------------------------------------------------
+
+//ゲームシーンオブジェクトヘッダ------------------
+#include "ObjHero.h"
+#include "ObjBatoru.h"
+#include "ObjBlock.h"
+#include "ObjEnemy.h"
+#include "ObjTime.h"
+#include "CObjheroattack.h"
+#include "ObjJunpEnemy.h"
+
+
+
+//------------------------------------------------
+
+//ゲームシーンクラスヘッダ------------------------
+#include "SceneTitle.h"
+#include "SceneBattle.h"
+#include "SceneItem.h"
+#include "SceneItem2.h"
+#include "SceneMap.h"
+#include "ShopSoubi.h"
+#include "ShopSoubi2.h"
+#include "SceneGameOver.h"
+
+//-----------------------------------------------
+
+//シーンスタートクラス---------------------------
+//ゲーム開始時のシーンクラス登録
+#define SET_GAME_START  CSceneTitle
+//-----------------------------------------------
---
+++ GA1AB/ColonyQuest/ColonyQuest/SceneGameOver.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "GameL\SceneObjManager.h"
+#include "GameL\SceneManager.h"
+
+
+using namespace GameL;
+
+class CSceneGameOver : public   CScene, CObj  //, CScene
+{
+public:
+	CSceneGameOver() {};
+	~CSceneGameOver() {};
+	void InitScene();	//実行
+	void Scene();		//初期化
+	void Action();
+	void Init();
+	void Draw();
+private:
+	float m_mou_x;//マウスの位置
+	float m_mou_y;//
+	bool m_mou_r; //ボタンの状態
+	bool m_mou_l; //
+};
